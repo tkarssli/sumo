a4e2d94661b0b53037b65372c3d3a87c67fa2997
Tam Kar <tkarssli@gmail.com>
2019-04-04 11:36:25 -0700

    Create register/login routes

---
+++ routes/api/users.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
+const User = require('../../models/User');
+
+const router = express.Router();
+
+
+router.post('/register', (req, res) => {
+  // Check if email already exists
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // return 400 if email exists
+        return res.status(400).json({
+          email: "A user has already registered with this email address"
+        })
+      } else {
+        // Create new User
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        })
+        console.log(req)
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => {
+                const payload = {id: user.id, name: user.name};
+
+                jwt.sign(
+                  payload,
+                  keys.secretOrKey,
+                  {expiresIn: 3600},
+                  (err, token) => {
+                    res.json({
+                      success: true,
+                      token: 'Bearer ' + token
+                    });
+                  });
+              })
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(passowrd, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, name: user.name};
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+
+          } else {
+            return res.status(400).json({password: 'Incorrect password'});
+          }
+        })
+    })
+})
+
+
+router.get("/test", (req, res) => res.json({
+  msg: "This is the users route"
+}));
+
+module.exports = router;
