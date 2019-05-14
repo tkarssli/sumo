fabe1a78216c2d06285734eed31dbfc9b63ee13a
Tamir Karssli <tkarssli@gmail.com>
2019-05-13 19:08:13 -0700

    Fix validation on password

--- validation/login.js
+++ validation/login.js
@@ -16,7 +16,7 @@ module.exports = function validateLoginInput(data) {
   }
 
   if(Validator.isEmpty(data.password)) {
-    errors.email = 'Password field is required';
+    errors.password = 'Password field is required';
   }
 
   return {
