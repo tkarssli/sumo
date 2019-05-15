4c20897342e7eac9e3b549b4adf9df10b24c653f
Tamir Karssli <tkarssli@gmail.com>
2019-05-15 15:22:24 -0700

    Add boolean for current active tournament

--- models/Tournament.js
+++ models/Tournament.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
 
 const TournamentSchema = new Schema({
+  running: Boolean,
   month: {
     type: Number,
     required: true
