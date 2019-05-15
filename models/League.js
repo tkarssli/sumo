d66ef62e3bbc95c3c4cd84de08fe0bdd48c7d51b
Tamir Karssli <tkarssli@gmail.com>
2019-05-15 15:22:48 -0700

    Add reference to tournament

--- models/League.js
+++ models/League.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
 
 const LeagueSchema = new Schema({
+  tournament: {type: Schema.Types.ObjectId, ref: 'tournaments'},
   players: [{ 
     playerId: { type: Schema.Types.ObjectId, ref: 'users'},
     wrestlers: [{type: Schema.Types.ObjectId, ref: 'wrestlers'}],
